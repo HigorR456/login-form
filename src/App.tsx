f7bc295160fb82109e04d2720f76c73aefa106d9
HigorR456 <higor.ruan456@gmail.com>
2022-12-28 21:55:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,44 @@ import './App.css'
 
 function App() {
 
+  const Error = () => {
+    setInvalidEmail()
+  }
+
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [invalidEmail, setInvalidEmail] = useState([])
+  const [login, setLogin] = useState(true)
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    console.log(value);
+    setEmail(value);
+  }
+
+  const handlePassword = (event) => {
+    const value = event.target.value;
+    console.log(value);
+    setPassword(value);
+  }
+
+  const submitBtn = () => {
+    let emailText = document.querySelector('#email').value;
+    console.log(emailText);
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailText)) { 
+      alert('Login Sucessful')
+      setInvalidEmail(() => {return (
+        <div className='errorMessage'></div>
+      )})
+      //setInvalidEmail(() => console.log('hey'))
+      return (true)
+    } alert("You have entered an invalid email address!")
+      setEmail('Invalid email')
+      return (false)
+  }
+
+  
 
 
   return (
@@ -12,20 +50,20 @@ function App() {
         <h1>Login Form</h1>
 
         {/* Show div below only if there is error message */}
-        <div className='errorMessage'></div>
+        
 
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input id={'email'} type={'email'} autoComplete='off' value={email} onChange={handleChange} />
         </div>
 
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input id={'password'} type={'password'} value={password} onChange={handlePassword} />
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          <button disabled={email === '' || password.length < 6} onClick={submitBtn}>Login</button>
         </div>
 
       </div>
