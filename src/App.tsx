d90f20bb70e198859f7faab92d741033dbbf86cc
HigorR456 <higor.ruan456@gmail.com>
2022-12-31 13:06:31 -0300

    Update
    Update App.tsx, update index.css, create login.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,53 @@
 import { useState } from 'react'
+import { login } from './login'
 import './App.css'
+
+// Instructions:
+// * You have an INCOMPLETE login form
+// * Adding new HTML elements is not allowed
+// * It is not allowed to use refs
+//
+// Tasks:
+// to do - The login button must trigger the login() function, imported at the top of this file, and pass the necessary data.
+// to do - Disable the Login button if the email is blank OR the password is less than 6 digits.
+// to do - Disable the Login button while you are performing login.
+// to do - Display a login() error message if Login fails. The message must be cleared at each new Login attempt.
+// to do - Show an alert if login is successful (javascript alert). Investigate the login() function to understand how to succeed the request.
 
 function App() {
 
-  const Error = () => {
-    setInvalidEmail()
-  }
-
-
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [invalidEmail, setInvalidEmail] = useState([])
-  const [login, setLogin] = useState(true)
+  const [error, setError] = useState(null)
+  const [isRequesting, setIsRequesting] = useState(false)
 
-  const handleChange = (event) => {
+  const handleChange = (event: any) => {
     const value = event.target.value;
     console.log(value);
     setEmail(value);
   }
 
-  const handlePassword = (event) => {
+  const handlePassword = (event: any) => {
     const value = event.target.value;
     console.log(value);
     setPassword(value);
   }
 
   const submitBtn = () => {
-    let emailText = document.querySelector('#email').value;
-    console.log(emailText);
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailText)) { 
-      alert('Login Sucessful')
-      setInvalidEmail(() => {return (
-        <div className='errorMessage'></div>
-      )})
-      //setInvalidEmail(() => console.log('hey'))
-      return (true)
-    } alert("You have entered an invalid email address!")
-      setEmail('Invalid email')
-      return (false)
+    console.log('submitted');
+
+    setError(null);
+    setIsRequesting(true);
+
+    let values = {email: email, password: password};
+    login(values)
+      .then(() => alert('Login Sucessful'))
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+      })
+      .finally(() => setIsRequesting(false));
   }
-
-  
-
 
   return (
     <div className='wrapper'>
@@ -50,7 +56,7 @@ function App() {
         <h1>Login Form</h1>
 
         {/* Show div below only if there is error message */}
-        
+        {error && <div className='errorMessage'>{error.message}</div>}
 
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
@@ -63,7 +69,9 @@ function App() {
         </div>
 
         <div className='button'>
-          <button disabled={email === '' || password.length < 6} onClick={submitBtn}>Login</button>
+          <button 
+            disabled={email === '' || password.length < 6 || isRequesting} 
+            onClick={submitBtn}>Login</button>
         </div>
 
       </div>
